c9052ad1a4cf8710046f917783eb72ec0175ec1e
Filippo Pesavento <pesaventofilippo@gmail.com>
2024-02-07 09:59:11 +0100

    Renew subscriptions every 24 hours

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     Thread(target=listener.run_server).start()
 
     while True:
-        # Possibly don't need to renew subscriptions - hub sends renew requests
-        # print("* Renewing subscriptions...")
-        # subscriber.subscribe_all()
+        print("* Renewing subscriptions...")
+        subscriber.subscribe_all()
         sleep(86400)
